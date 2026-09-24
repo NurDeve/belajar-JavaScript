f8f4f92671a2b64873ad42e9628f08c57b1851cb
NurDeve <bisaaku441@gmail.com>
2026-09-24 22:10:48 +0700

    menambahkan file buat belajar function

---
+++ belajarjava12.js
@@ -0,0 +1,10 @@
+
+function hitungPerkalian(nilai) {
+    for(let i = 1; i <= 5; i++){
+    let hasil = i * nilai;
+       console.log(nilai + ' * ' + i + ' = ' + hasil)
+}
+ 
+}
+
+hitungPerkalian(5);
